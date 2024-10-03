a117d4e55f24bc9fb977845edcc6f8340cd94a92
Jeffrey <kalavathy721945@gmail.com>
2024-10-03 18:17:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,17 +20,17 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/Anime_Weekends'>Anime Weekends</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/Jeffrey_Weekends'>Jeffrey Weekends</a>
 
-<b>♻️ Bot Made By :</b> @Madflix_Bots"""
+<b>♻️ Bot Made By :</b> @Anime_Weekends"""
 
     DONATE_TXT = """
 <b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> 
+<b>🛍 OWNER ID:</b> <code>@Jeffrey_Weekends</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
@@ -60,7 +60,6 @@ For Example :-
 
 
 
-# Jishu Developer 
+# @Jeffrey_Weekends
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
+# Telegram Channel @Anime_Weekends
